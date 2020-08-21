f12db7b54b5cdad04e04b15df77ee8d19aea3008
MrPowers <matthewkevinpowers@gmail.com>
2020-08-21 13:53:30 -0500

    Add a Dask DataFrame generator that can be customized

--- farsante/dask_generators.py
+++ farsante/dask_generators.py
@@ -2,6 +2,11 @@ from farsante.pandas_generators import *
 import dask.dataframe as dd
 
 def quick_dask_df(cols, num_rows, npartitions=1):
-    pandas_df = quick_pandas_df(cols, num_rows)
-    return dd.from_pandas(pandas_df, npartitions=npartitions)
+    df = quick_pandas_df(cols, num_rows)
+    return dd.from_pandas(df, npartitions=npartitions)
+
+
+def dask_df(funs, num_rows, npartitions=1):
+    df = pandas_df(funs, num_rows)
+    return dd.from_pandas(df, npartitions=npartitions)
 
--- tests/test_dask_generators.py
+++ tests/test_dask_generators.py
@@ -11,3 +11,14 @@ def test_quick_dask_df():
     df = farsante.quick_dask_df(['first_name', 'last_name'], 3)
     # print(df.head(3))
     assert len(df.index) == 3
+
+
+def test_dask_df():
+    def rand_int(min_int, max_int):
+        def f():
+            return random.randint(min_int, max_int)
+        return f
+    mx = Person('es-mx')
+    df = farsante.dask_df([mx.first_name, mx.last_name, rand_int(1000, 2000)], 5)
+    # print(df.head(3))
+    assert len(df.index) == 5
