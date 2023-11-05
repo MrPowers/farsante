c0a6150bcae80149c150e3e954e3edebec9e752b
SemyonSinchenko <semyonsinchenko@yahoo.com>
2023-11-05 16:02:19 +0100

    Refactoring of main.rs
     Changes to be committed:
    	modified:   h2o-data-rust/src/main.rs

--- h2o-data-rust/src/main.rs
+++ h2o-data-rust/src/main.rs
@@ -1,5 +1,5 @@
-use std::{fs, path::PathBuf};
 use std::io::prelude::*;
+use std::{fs, path::PathBuf};
 
 use clap::Parser;
 use kdam::tqdm;
@@ -7,97 +7,88 @@ use rand::distributions::{Distribution, Uniform};
 use rand::prelude::*;
 use rand_chacha::ChaCha8Rng;
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct CliArgs {
-    // Number of rows
+    /// Number of rows
     #[arg(long)]
     n: u64,
-    // Number of keys
+    /// Number of keys
     #[arg(long)]
     k: u64,
-    // Number of NAs
+    /// Number of NAs
     #[arg(long, default_value_t = 0)]
     nas: u8,
-    // Random seed
+    /// Random seed
     #[arg(long, default_value_t = 108)]
     seed: u64,
 }
 
-struct Row {
-    id1: String,
-    id2: String,
-    id3: String,
-    id4: u64,
-    id5: u64,
-    id6: u64,
-    v1: u8,
-    v2: u8,
-    v3: f64,
+struct RowGenerator {
+    nas: u8,
+    distr_k: Uniform<u64>,
+    distr_nk: Uniform<u64>,
+    distr_5: Uniform<u8>,
+    distr_15: Uniform<u8>,
+    distr_float: Uniform<f64>,
+    distr_nas: Uniform<u8>,
+    rng: ChaCha8Rng,
 }
 
-impl Row {
-    fn generate(
-        rng: &mut ChaCha8Rng,
-        distr_k: Uniform<u64>,
-        distr_nk: Uniform<u64>,
-        distr_5: Uniform<u8>,
-        distr_15: Uniform<u8>,
-        distr_float: Uniform<f64>,
-    ) -> Self {
-        Row {
-            id1: format!("id{:03}", distr_k.sample(rng)),
-            id2: format!("id{:03}", distr_k.sample(rng)),
-            id3: format!("id{:010}", distr_nk.sample(rng)),
-            id4: distr_k.sample(rng),
-            id5: distr_k.sample(rng),
-            id6: distr_nk.sample(rng),
-            v1: distr_5.sample(rng),
-            v2: distr_15.sample(rng),
-            v3: distr_float.sample(rng),
+impl RowGenerator {
+    fn new(n: u64, k: u64, nas: u8, seed: u64) -> Self {
+        RowGenerator {
+            nas,
+            distr_k: Uniform::<u64>::try_from(1..=k).unwrap(),
+            distr_nk: Uniform::<u64>::try_from(1..=(n / k)).unwrap(),
+            distr_5: Uniform::<u8>::try_from(1..=5).unwrap(),
+            distr_15: Uniform::<u8>::try_from(1..=15).unwrap(),
+            distr_float: Uniform::<f64>::try_from(0.0..=100.0).unwrap(),
+            distr_nas: Uniform::<u8>::try_from(0..=100).unwrap(),
+            rng: ChaCha8Rng::seed_from_u64(seed),
         }
     }
 
-    fn get_csv_header() -> String {
+    fn get_csv_header(&self) -> String {
         "id1,id2,id3,id4,id5,id6,v1,v2,v3\n".to_string()
     }
 
-    fn get_csv_row(&self, nas_distr: Uniform<u8>, rng: &mut ChaCha8Rng, nas: u8) -> String {
+    fn get_csv_row(&mut self) -> String {
         format!(
             "{},{},{},{},{},{},{},{},{}",
-            if nas_distr.sample(rng) >= nas {
-                self.id1.as_str()
-            } else {
-                ""
-            },
-            if nas_distr.sample(rng) >= nas {
-                self.id2.as_str()
-            } else {
-                ""
-            },
-            if nas_distr.sample(rng) >= nas {
-                self.id3.as_str()
-            } else {
-                ""
-            },
-            if nas_distr.sample(rng) >= nas {
-                format!("{}", self.id4)
+            if self.distr_nas.sample(&mut self.rng) >= self.nas {
+                format!("id{:03}", self.distr_k.sample(&mut self.rng))
             } else {
                 "".to_string()
             },
-            if nas_distr.sample(rng) >= nas {
-                format!("{}", self.id5)
+            if self.distr_nas.sample(&mut self.rng) >= self.nas {
+                format!("id{:03}", self.distr_k.sample(&mut self.rng))
             } else {
                 "".to_string()
             },
-            if nas_distr.sample(rng) >= nas {
-                format!("{}", self.id6)
+            if self.distr_nas.sample(&mut self.rng) >= self.nas {
+                format!("id{:010}", self.distr_nk.sample(&mut self.rng))
             } else {
                 "".to_string()
             },
-            self.v1,
-            self.v2,
-            self.v3
+            if self.distr_nas.sample(&mut self.rng) >= self.nas {
+                format!("{}", self.distr_k.sample(&mut self.rng))
+            } else {
+                "".to_string()
+            },
+            if self.distr_nas.sample(&mut self.rng) >= self.nas {
+                format!("{}", self.distr_k.sample(&mut self.rng))
+            } else {
+                "".to_string()
+            },
+            if self.distr_nas.sample(&mut self.rng) >= self.nas {
+                format!("{}", self.distr_nk.sample(&mut self.rng))
+            } else {
+                "".to_string()
+            },
+            self.distr_5.sample(&mut self.rng),
+            self.distr_15.sample(&mut self.rng),
+            self.distr_float.sample(&mut self.rng),
         )
     }
 }
@@ -114,12 +105,6 @@ fn pretty_sci(num: u64) -> String {
 
 fn main() {
     let args = CliArgs::parse();
-    let distr_k = Uniform::<u64>::try_from(1..=args.k).unwrap();
-    let distr_nk = Uniform::<u64>::try_from(1..=args.k).unwrap();
-    let distr_5 = Uniform::<u8>::try_from(1..=5).unwrap();
-    let distr_15 = Uniform::<u8>::try_from(1..=15).unwrap();
-    let distr_float = Uniform::<f64>::try_from(0.0..=100.0).unwrap();
-    let nas_distr = Uniform::<u8>::try_from(0..=100).unwrap();
 
     let output_file_name = format!(
         "G1_{}_{}_{}.csv",
@@ -127,9 +112,12 @@ fn main() {
         pretty_sci(args.k),
         args.nas
     );
-    let mut rng = ChaCha8Rng::seed_from_u64(args.seed);
     println!("Write output to {}", output_file_name);
-    let _ = fs::write(PathBuf::from(&output_file_name), Row::get_csv_header());
+    let mut row_generator = RowGenerator::new(args.n, args.k, args.nas, args.seed);
+    let _ = fs::write(
+        PathBuf::from(&output_file_name),
+        row_generator.get_csv_header(),
+    );
     let mut file = fs::OpenOptions::new()
         .append(true)
         .write(true)
@@ -137,8 +125,7 @@ fn main() {
         .unwrap();
 
     for _ in tqdm!(0..args.n) {
-        let row = Row::generate(&mut rng, distr_k, distr_nk, distr_5, distr_15, distr_float);
-        if let Err(e) = writeln!(file, "{}", row.get_csv_row(nas_distr, &mut rng, args.nas)) {
+        if let Err(e) = writeln!(file, "{}", row_generator.get_csv_row()) {
             eprintln!("couldn't write to file: {}", e);
         }
     }
