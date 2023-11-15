9d385eea756668433d4c6066e8721f683613a3e6
jeffbrennan <jeffbrennan10@gmail.com>
2023-11-15 12:59:04 -0500

    handle all dataset types

--- farsante/h2o_dataset_create.py
+++ farsante/h2o_dataset_create.py
@@ -1,21 +1,65 @@
-from h2o_data_rust import generate_groupby_csv
+from h2o_data_rust import generate_csv_py
 
 
 def pretty_sci(n: int) -> str:
     if n == 0:
         return "NA"
 
+    # format in scientific notation and remove +
     formatted_num = f"{n:.0e}".replace("+", "")
     e_value = int(formatted_num.split("e")[1])
-    if e_value < 10:
-        formatted_num = formatted_num.replace("0", "")
-    return formatted_num
+
+    if e_value >= 10:
+        return formatted_num
+
+    elif e_value == 0:
+        return formatted_num.replace("00", "0")
+
+    elif e_value < 10:
+        return formatted_num.replace("0", "")
+
+    else:
+        raise ValueError("Unexpected value following e")
 
 
-def generate_dataset(dataset_type: str, n: int, k: int, nas: int, seed: int) -> None:
-    if dataset_type == "groupby":
-        output_name = f"G1_{pretty_sci(n)}_{pretty_sci(n)}_{k}_{nas}.csv"
-        generate_groupby_csv(output_name, n, k, nas, seed)
+# TODO: come back to this - focus on verifying n arguments, file names
+def generate_dataset(n: int, k: int, nas: int, seed: int, ds_type: str) -> None:
+    output_names = {
+        "groupby": "G1_{n}_{n}_{k}_{nas}.csv",
+        "join_big": "J1_{n}_{n}_NA.csv",
+        "join_big_na": "J1_{n}_{n}_{nas}.csv",
+        "join_small": "J1_{n}_{n_divided}_{nas}.csv",
+        "join_medium": "J1_{n}_{n_divided}_{nas}.csv",
+    }
+
+    divisors = {
+        "groupby": 1,
+        "join_big": 1,
+        "join_big_na": 1,
+        "join_small": 1_000_000,
+        "join_medium": 1_000,
+    }
+
+    n_divisor = divisors[ds_type]
+    n_divided = n // n_divisor
+    output_name = output_names[ds_type].format(
+        n=pretty_sci(n), n_divided=pretty_sci(n_divided), k=k, nas=nas
+    )
+    print(output_name)
+    generate_csv_py(
+        output_name=output_name,
+        n=n,
+        k=k,
+        nas=nas,
+        seed=seed,
+        ds_type=ds_type,
+    )
 
 
-generate_dataset(dataset_type="groupby", n=1000000, k=10, nas=10, seed=10)
+generate_dataset(
+    n=10_000_000,
+    k=10,
+    nas=10,
+    seed=10,
+    ds_type="groupby",
+)
