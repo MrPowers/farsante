a7248ccee8e0ee08a81d280819a6322554e2745b
jeffbrennan <jeffbrennan10@gmail.com>
2023-11-18 22:24:32 -0500

    fix copilot help messages

--- farsante/h2o_dataset_create_all.py
+++ farsante/h2o_dataset_create_all.py
@@ -3,19 +3,19 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Optional app description")
+    parser = argparse.ArgumentParser(description="Generate all h2o datasets")
     parser.add_argument(
         "--n", type=int, help="Number of rows to generate", required=True
     )
 
     parser.add_argument(
-        "--k", type=int, help="Number of columns to generate", required=True
+        "--k", type=int, help="Number of unique keys in dataset", required=True
     )
 
     parser.add_argument(
         "--nas",
         type=int,
-        help="Number of nas to generate",
+        help="Percent of null values to generate (0-100) [default: 0]",
         required=False,
         default=0,
     )
@@ -23,7 +23,7 @@ def main():
     parser.add_argument(
         "--seed",
         type=int,
-        help="Seed for random number generator",
+        help="Seed for random number generator [default: 42]",
         required=False,
         default=42,
     )
